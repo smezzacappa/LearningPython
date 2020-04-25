1babe7e5dbba678e7367738aa25fffc197d709bb
Steven <stevenmezzacappa@Stevens-MacBook-Pro.local>
2020-04-25 00:16:06 -0400

    Combining Repos

---
+++ Strings/app.py
@@ -0,0 +1,20 @@
+course = 'Python for "Beginners"'
+course_two= "Python's Course for Beginners"
+print(course)
+
+message = ''' 
+ Hi John, 
+ 
+ Here is our first email to you.
+ 
+ Thanks, 
+ Steven Mezzacappa
+ '''
+print(message)
+
+course_name = 'Python for Beginners'
+another = course[:]
+print(another)
+
+name = 'Jennifer'
+print(name[1:-1])
